680bf67ca9614f060ce7a1ae908299b3512a2f00
Brian Spiering <bspiering@gmail.com>
2018-05-17 11:02:49 -0700

    Fix line break bug

--- make_teams.py
+++ make_teams.py
@@ -22,37 +22,38 @@ def make_teams(students: list, weeks: int=6, days: int=2, path: str='teams'):
             with open(path+'/'+file_name, 'w') as out_file:
                 
                 line_break_counter = 0
+                range_current = range(1, n_students, 2)
                 
                 if n_students % 2 == 0:
                     out_file.write("| Driver | Navigator | \n")
                     out_file.write("|--------|-----------| \n")
-                    for i in range(1, n_students, 2):
-                        out_file.write('|{}|{}|\n'.format(students[i - 1], students[i]))
+                    for i in range_current:
+                        out_file.write(f'|{students[i - 1]}|{students[i]}|\n')
                         
                         # Add line break for 4-5 person RAT teams
                         line_break_counter += 1
                         
-                        if (line_break_counter % 2 == 0): 
+                        if (line_break_counter % 2 == 0) and (i != range_current[-1]): 
                              out_file.write('| | |\n')
                             
                 else: # If there is an odd number of students, add triple "pair" 
                         out_file.write("| Driver | Navigator 1 | Navigator 2 | \n")
                         out_file.write("|--------|-----------|------------| \n")
                         loop_num, total_loops = 0, int(n_students/2)
-                        for i in range(1, n_students, 2):
+                        for i in range_current:
                             loop_num += 1
 
                             if loop_num != total_loops:
-                                out_file.write('| {} | {} | |\n'.format(students[i - 1], students[i]))
+                                out_file.write(f'| {students[i - 1]} | {students[i]} | |\n')
                             else:
                                 odd_person_out = students[-1:n_students]
-                                out_file.write('| {} | {} | {} |\n'.format(students[i - 1], students[i], odd_person_out[0]))
+                                out_file.write(f'| {students[i - 1]} | {students[i]} | {odd_person_out[0]} |\n')
                             
                             # Add line break for 4-5 person quiz teams
                             line_break_counter += 1
                         
-                            if (line_break_counter % 2 == 0): 
-                                 out_file.write('| | |\n')
+                            if (line_break_counter % 2 == 0) and (i != range_current[-1]): 
+                                 out_file.write('| | | |\n')
 
                 out_file.write("  \n")
 
