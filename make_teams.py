710d848bd66b08fd52131ed7d829e3dd8787804f
Brian Spiering <bspiering@gmail.com>
2018-06-01 20:12:35 -0700

    Label teams and pairs

--- make_teams.py
+++ make_teams.py
@@ -17,43 +17,58 @@ def make_teams(students: list, weeks: int=6, days: int=2, path: str='teams'):
     
     for week in range(1, weeks+1):
         for day in range(1, days+1):
+            
             random.shuffle(students)
             file_name = "teams_"+str(week)+"_"+str(day)+".md"
+            
             with open(path+'/'+file_name, 'w') as out_file:
                 
-                line_break_counter = 0
+                line_break_counter, team_counter, pair_counter = 0, 0, 0
                 range_current = range(1, n_students, 2)
                 
                 if n_students % 2 == 0:
-                    out_file.write("| Driver | Navigator | \n")
-                    out_file.write("|--------|-----------| \n")
+
                     for i in range_current:
-                        out_file.write(f'|{students[i - 1]}|{students[i]}|\n')
+
+                        if (line_break_counter % 2 == 0):
+                            out_file.write(f"|     | Team {team_counter} |     |     |\n")
+                            out_file.write("|:----:|:---:|:---:|:---:| \n")
+
+                        out_file.write(f'| Pair {pair_counter} |{students[i - 1]}|{students[i]}|\n')
                         
-                        # Add line break for 4-5 person RAT teams
-                        line_break_counter += 1
+                        pair_counter += 1
                         
+                        line_break_counter += 1 # Add line break for 4-5 person teams
+
                         if (line_break_counter % 2 == 0) and (i != range_current[-1]): 
-                             out_file.write('| | |\n')
+                             out_file.write('\n')
+                             team_counter += 1
                             
                 else: # If there is an odd number of students, add triple "pair" 
-                        out_file.write("| Driver | Navigator 1 | Navigator 2 | \n")
-                        out_file.write("|--------|-----------|------------| \n")
-                        loop_num, total_loops = 0, int(n_students/2)
-                        for i in range_current:
-                            loop_num += 1
-
-                            if loop_num != total_loops:
-                                out_file.write(f'| {students[i - 1]} | {students[i]} | |\n')
-                            else:
-                                odd_person_out = students[-1:n_students]
-                                out_file.write(f'| {students[i - 1]} | {students[i]} | {odd_person_out[0]} |\n')
-                            
-                            # Add line break for 4-5 person quiz teams
-                            line_break_counter += 1
+                    loop_num, total_loops  = 0, int(n_students/2)
+                    
+                    for i in range_current:
                         
-                            if (line_break_counter % 2 == 0) and (i != range_current[-1]): 
-                                 out_file.write('| | | |\n')
+                        if (line_break_counter % 2 == 0):
+                            out_file.write(f"|     | Team {team_counter} |     |     |\n")
+                            out_file.write("|:----:|:---:|:---:|:---:| \n")
+                        
+                        loop_num += 1
+
+                        if loop_num != total_loops:
+                            out_file.write(f'| Pair {pair_counter} | {students[i - 1]} | {students[i]} | |\n')
+                           
+                        else:
+
+                            odd_person_out = students[-1:n_students]
+                            out_file.write(f'| Pair {pair_counter} | {students[i - 1]} | {students[i]} | {odd_person_out[0]} |\n')
+                        
+                        pair_counter += 1
+                        line_break_counter += 1 # Add line break for 4-5 person  teams
+                    
+                        if (line_break_counter % 2 == 0) and (i != range_current[-1]): 
+                             out_file.write('\n')
+                             team_counter += 1
 
                 out_file.write("  \n")
 
